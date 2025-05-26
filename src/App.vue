1dca1edfe74d01cb42a33795ea5b3283daa5880f
khun ly <khun.ly@bstorm.be>
2025-05-27 00:14:43 +0200

    v2

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import Hello from './components/Hello.vue';
 </script>
 
 <template>
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import Hello from './components/Hello.vue';
 
 const app = createApp(App);
+app.component('Hello', Hello)
 app.mount('#app');
